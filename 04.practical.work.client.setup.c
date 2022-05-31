4817b06100d34a1825fd1ff56a47c4d4aa520f60
hatrang010802 <tranggkoyy2k2@gmail.com>
2022-05-31 15:39:04 +0700

    update pw4

--- 04.practical.work.client.setup.c
+++ 04.practical.work.client.setup.c
@@ -44,7 +44,9 @@ int main(int argc, char *argv[]){
     }
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;//ipv4
-    memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length); saddr.sin_port = htons(port);
+    memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+    saddr.sin_port = htons(port);
+    
     if (connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0){
         printf("Cannot connect\n");
         return 1;
